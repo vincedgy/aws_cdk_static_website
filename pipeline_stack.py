625a4fc2812255795982c64115ac70e86c8f569a
Vincent <vincent.dagoury@gmail.com>
2022-08-28 21:34:20 +0200

    Fixing Pipeline again

--- pipeline_stack.py
+++ pipeline_stack.py
@@ -26,13 +26,17 @@ class StaticSitePipelineStack(Stack):
             synth=pipelines.ShellStep(
                 "Synth",
                 input=pipelines.CodePipelineSource.code_commit(repo, "main"),
-                commands=[
+                install_commands=[
                     "npm install -g aws-cdk",  # Installs the cdk cli on Codebuild
                     # Instructs Codebuild to install required packages
                     # "pip install --upgrade pip",
                     "pip install -r requirements.txt",
-                    "cd ./website && npm ci && npm run build && cd ..",
-                    "cdk synth",
+                ],
+                commands=[
+                    "export STARTING_DIR=$(pwd)"
+                    "cd ./website && npm ci && npm run build",
+                    "cd $STARTING_DIR",
+                    "cdk synth --ci --app cdk.out StaticSitePipelineStack --ci",
                 ]
             ),
         )
